d4efe5682180b53b43391bf85430dabd6dba6348
Julian DiCesare <jrdicesare@protonmail.com>
2018-01-11 21:34:42 -0700

    determine pollrate

--- run/run.go
+++ run/run.go
@@ -3,6 +3,7 @@ package run
 import (
 	"log"
 	"path/filepath"
+	"time"
 
 	"github.com/marcsauter/single"
 	"gopkg.in/natefinch/lumberjack.v2"
@@ -20,6 +21,32 @@ func setLogOutput(logName string) *log.Logger {
 		Compress:   true,
 	})
 	return logger
+}
+
+func getPollrate(runConfigs []RunConfig) (result time.Duration) {
+	if len(runConfigs) > 1 {
+		// Euclid algo
+		gcd := func(x, y int) int {
+			for y != 0 {
+				x, y = y, x%y
+			}
+			return x
+		}
+		// This is the result for only 2 integers
+		rate := gcd(runConfigs[0].Rotations.Frequency, runConfigs[1].Rotations.Frequency)
+		result = time.Duration(rate)
+		// for loop in case there're more than 2 ints
+		for j := 3; j <= len(runConfigs)-1; j++ {
+			rate = gcd(rate, runConfigs[j].Rotations.Frequency)
+			result = time.Duration(rate)
+		}
+	} else {
+		if len(runConfigs) < 1 {
+			return
+		}
+		result = time.Duration(runConfigs[0].Rotations.Frequency)
+	}
+	return
 }
 
 func Daemonize() {
